de86613d541f59a7f116ee9525a6c439ad149519
Bibit Kunwar <kunwar.bibit7@gmail.com>
2022-06-26 18:28:40 +0545

    Form added

---
+++ Practice/students-mgmt/utils/db.php
@@ -0,0 +1,12 @@
+<?php 
+
+$serverName = "localhost";
+$userName = "root";
+$password = "root";
+$databaseName = "studentmgmt";
+
+$conn = new mysqli($serverName, $userName, $password, $databaseName);
+
+if($conn->connect_error) {
+	die("Connection failed!");
+}
